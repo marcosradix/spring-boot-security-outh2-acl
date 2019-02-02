55c94d787eae78373eff8671d8d376faaf7a5ae7
Marcos Ferreira <marcosradix@gmail.com>
2019-02-02 10:11:15 -0300

    primeiro commit

---
+++ src/main/java/br/com/workmade/springbootsecurityouth2acl/SpringBootSecurityOuth2AclApplication.java
@@ -0,0 +1,14 @@
+package br.com.workmade.springbootsecurityouth2acl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityOuth2AclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityOuth2AclApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/br/com/workmade/springbootsecurityouth2acl/SpringBootSecurityOuth2AclApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.workmade.springbootsecurityouth2acl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSecurityOuth2AclApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
